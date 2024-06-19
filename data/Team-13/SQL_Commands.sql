c7987b7de36d3a4d09395eca1ac4438e89490d73
Ernani Fantinatti <ernanif@fantinatti.com>
2024-06-18 22:10:30 -0400

    Updated SQL Historical file.

--- data/Team-13/SQL_Commands.sql
+++ data/Team-13/SQL_Commands.sql
@@ -1,3 +1,7 @@
 --ALTER TABLE "E-commerce Customer Behavior" RENAME TO E_Comm_Customer_Behavior;
 
-SELECT * FROM E_Comm_Customer_Behavior;
+--Initial evaluation
+--SELECT * FROM E_Comm_Customer_Behavior;
+
+-- List tables
+SELECT name FROM sqlite_master WHERE type='table';
